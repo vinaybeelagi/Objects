2ad2583edfa02c4e65617569130ea3895128863d
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-11 17:35:52 +0530

    task-Create new Calculator

--- object.js
+++ object.js
@@ -184,8 +184,29 @@
 //   john.sayHi(); // My name is: John
 
 //task-Two functions – one object:
-let obj = {};
-function A() {return obj;}
-function B() {return obj;}
-console.log (new A() === new B());
-  
+// let obj = {};
+// function A() {return obj;}
+// function B() {return obj;}
+// console.log (new A() === new B());
+
+//Create new Calculator
+function Calculator() {
+    this.read = function() {
+        this.a = +prompt('a?',0);
+        this.b = +prompt('b?',0);
+    };
+
+    this.sum = function() {
+     return this.a + this.b;
+    };
+
+    this.mul = function() {
+        return this.a*this.b;
+    };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert("sum=" +calculator.sum());
+alert("mul=" +calculator.mul());
