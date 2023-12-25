737ba167c3f4ba5d0ecba7f5fbf193c73ec59536
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-25 18:59:50 +0530

    Object.assign()

--- objectMDN.js
+++ objectMDN.js
@@ -31,3 +31,16 @@ a2.constructor === Array; // true
 const n = 3;
 n.constructor === Number; // true
 
+
+// Object.assign()
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target);
+// Expected output: true
+
