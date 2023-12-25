1e5665499bfc29da2a5731733162f29fd541ade2
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-25 18:41:37 +0530

    object literals create plain objects.

--- objectMDN.js
+++ objectMDN.js
@@ -13,3 +13,21 @@ console.log(typeof bigintObj); // "object"
 
 const symbolObj = Object(Symbol("foo"));
 console.log(typeof symbolObj); // "object"
+
+
+// object literals create plain objects.
+const o1 = {};
+o1.constructor === Object; // true
+
+const o2 = new Object();
+o2.constructor === Object; // true
+
+const a1 = [];
+a1.constructor === Array; // true
+
+const a2 = new Array();
+a2.constructor === Array; // true
+
+const n = 3;
+n.constructor === Number; // true
+
